0f6e00615dccc5274ceaa71a1703e01b9be3f6a5
SDHINSA <sohanad@icloud.com>
2026-04-28 22:17:31 -0600

    Connected and created the db

--- storage/db.js
+++ storage/db.js
@@ -3,9 +3,9 @@ import { Pool } from "pg";
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "<role_name>",
-  database: "CHANGE THIS",
-  password: "<role_password>",
-  port: 5432 // The default port
+  host: process.env.HOST, // or wherever the db is hosted
+  user: process.env.ROLE_NAME,
+  database: process.env.DB,
+  password: process.env.ROLE_PW,
+  port: process.env.DB_PORT  // The default port
 });
