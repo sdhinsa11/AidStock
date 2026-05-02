bbbc08dad6b2b10256e959ffc2c8778004f529a0
SDHINSA <sohanad@icloud.com>
2026-05-02 09:09:55 -0600

    Making a plan

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -4,6 +4,8 @@ import { getAllItems  } from "../controllers/itemsController.js";
 
 const itemRouter = Router();
 
+// filter the categories here too in this router
+
 itemRouter.get("/", getAllItems);
 
 // itemRouter.get("/:id", <controllers></controllers>);
